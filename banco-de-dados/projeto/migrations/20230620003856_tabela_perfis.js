2f8e17f6595d6933be8ed8dc89d926bc8a84cbbd
mabiamabia <duartecostap@gmail.com>
2023-07-23 10:22:51 -0300

    feat: connect to db

--- banco-de-dados/projeto/migrations/20230620003856_tabela_perfis.js
+++ banco-de-dados/projeto/migrations/20230620003856_tabela_perfis.js
@@ -3,11 +3,19 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.createTable("perfis", (Table) => {
-    table.increments("id").primary();
-    table.string("nome").notNull().unique();
-    table.string("rotulo").notNull();
-  });
+  return knex.schema
+    .createTable("perfis", (Table) => {
+      table.increments("id").primary();
+      table.string("nome").notNull().unique();
+      table.string("rotulo").notNull();
+    })
+    .then(function () {
+      return knex("perfis").insert([
+        { nome: "comum", rotulo: "Comum" },
+        { nome: "admin", rotulo: "Administrador" },
+        { nome: "master", rotulo: "Master" },
+      ]);
+    });
 };
 
 /**
--- banco-de-dados/projeto/migrations/20230620003929_tabela_usuario.js
+++ banco-de-dados/projeto/migrations/20230620003929_tabela_usuario.js
@@ -2,14 +2,21 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("usuarios", (table) => {
+    table.increments("id").primary();
+    table.string("nome").notNull();
+    table.string("email").notNull().unique();
+    table.string("senha", 60).notNull();
+    table.boolean("ativo").notNull().defaultTo(true);
+    table.timestamp("data_criacao").defaultTo(knex.fn.now());
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("usuarios");
 };
