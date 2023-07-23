9ea0ed520cbba901bcbc79672455612b6e59a6c6
mabiamabia <duartecostap@gmail.com>
2023-07-23 09:16:17 -0300

    feat: migrations

--- banco-de-dados/projeto/migrations/20230620003856_tabela_perfis.js
+++ banco-de-dados/projeto/migrations/20230620003856_tabela_perfis.js
@@ -2,14 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("perfis", (Table) => {
+    table.increments("id").primary();
+    table.string("nome").notNull().unique();
+    table.string("rotulo").notNull();
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("perfis");
 };
